b9474bdb3f6b2ada350337f75eb1f22001046b5f
Nivrith Gomatam <nivrith@Nivs-Work-iMac.local>
2019-03-21 12:08:33 +1030

    Bored of agnoster

--- tilde/zshrc.zsh
+++ tilde/zshrc.zsh
@@ -16,7 +16,7 @@ export ZSH="/Users/nivrith/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="agnoster"
+ZSH_THEME="robbyrussel"
 
 # ZSH_THEME_RANDOM_CANDIDATES=(
 #   "robbyrussell"
