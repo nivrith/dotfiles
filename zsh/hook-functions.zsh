4717c522d9c9a386e500882511470a16183fa2e9
Nivrith <nivrith@lumi.media>
2019-04-05 10:00:33 +1030

    Cleanup

--- zsh/hook-functions.zsh
+++ zsh/hook-functions.zsh
@@ -11,12 +11,12 @@ nvm_autouse() {
 
 PREV_PWD=$PWD
 if [[ -f ".nvmrc" ]]; then
-   nvm use &>/dev/null
+   nvm use 
    NVM_DIRTY=true
 elif [[ $NVM_DIRTY = true ]]; then
    nvm use default
    NVM_DIRTY=false
 fi
 }
-nvm_autouse
+nvm_autouse &>/dev/null
 chpwd_functions=(${chpwd_functions[@]} "nvm_autouse")
