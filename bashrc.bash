c13f8b72ed13e2b8ff3a13b924b503478438769b
Nivrith <nivrith.mandayam@gmail.com>
2019-04-10 12:01:51 +0930

    AutoNvm for bash

--- bashrc.bash
+++ bashrc.bash
@@ -1,3 +1,26 @@
 source ~/dotfiles/bash/env
 source ~/dotfiles/bash/config
 source ~/dotfiles/bash/aliases
+#
+# Run 'nvm use' automatically every time there's
+# a .nvmrc file in the directory. Also, revert to default
+# version when entering a directory without .nvmrc
+#
+enter_directory() {
+if [[ $PWD == $PREV_PWD ]]; then
+   return
+fi
+
+
+PREV_PWD=$PWD
+if [[ -f ".nvmrc" ]]; then
+   nvm use
+   NVM_DIRTY=true
+elif [[ $NVM_DIRTY = true ]]; then
+   nvm use default
+   NVM_DIRTY=false
+fi
+}
+
+
+export PROMPT_COMMAND=enter_directory
